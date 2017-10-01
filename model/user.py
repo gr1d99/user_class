d9db761cfe0bcdb44866374960e7dde0345201f9
gr1d99 <gideonkimutai9@gmail.com>
2017-10-01 09:37:40 +0300

    Add coveralls support

--- model/user.py
+++ model/user.py
@@ -4,3 +4,11 @@
 class User(object):
     """User class"""
     USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    PASSWORD_FIELD = 'password'
+
+    def __init__(self):
+        """Initialize class attributes"""
+        self.username = None
+        self.email = None
+        self.password = None
