baaf13c3b0c6709a7b6205adf65d541d4902b189
gr1d99 <gideonkimutai9@gmail.com>
2017-10-01 10:10:16 +0300

    [FEATURE]Add more methods

--- model/user.py
+++ model/user.py
@@ -9,6 +9,24 @@ class User(object):
 
     def __init__(self):
         """Initialize class attributes"""
+        # set all attributes to None because we will validate first
+        # before assigning values
         self.username = None
         self.email = None
         self.password = None
+
+    @property
+    def get_username(self):
+        """
+        gets username value
+        :return:
+        """
+        return getattr(self, User.USERNAME_FIELD)
+
+    @property
+    def get_email(self):
+        """
+        gets email value
+        :return:
+        """
+        return getattr(self, User.EMAIL_FIELD)
