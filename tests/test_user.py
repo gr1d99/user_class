ff8a410ec1c125692b68e0141d1d3937895c024a
gr1d99 <gideonkimutai9@gmail.com>
2017-10-01 10:10:57 +0300

    [TESTS]Add more tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,6 @@
+"""Contains all tests for the User class"""
+
+
 import unittest
 from model.user import User
 
@@ -6,27 +9,65 @@ class TestUser(unittest.TestCase):
     """Tests all functionalities"""
 
     def setUp(self):
+        """initialize User class and make it available to the test class"""
         self.user = User()
 
     #  -->start of User attributes and methods<--
     def test_has_username_field(self):
+        """
+        assert whether User class has attribute USERNAME_FIELD
+        :return:
+        """
         self.assertTrue(hasattr(self.user, 'USERNAME_FIELD'))
 
     def test_has_email_field(self):
+        """
+        assert if User class has attribute EMAIL_FIELD
+        :return:
+        """
         self.assertTrue(hasattr(self.user, 'EMAIL_FIELD'))
 
     def test_has_password_field(self):
+        """
+        assert if User class has PASSWORD_FIELD
+        :return:
+        """
         self.assertTrue(hasattr(self.user, 'PASSWORD_FIELD'))
 
     def test_username_default_is_none(self):
+        """
+        assert if User instance attribute username default value is None
+        :return:
+        """
         self.assertTrue(getattr(self.user, self.user.USERNAME_FIELD) is None)
 
     def test_email_default_is_none(self):
+        """
+        assert if User instance attribute email default value is None
+        :return:
+        """
         self.assertTrue(getattr(self.user, self.user.EMAIL_FIELD) is None)
 
     def test_password_default_is_none(self):
+        """
+        assert if User instance attribute password default value is None
+        :return:
+        """
         self.assertTrue(getattr(self.user, self.user.PASSWORD_FIELD) is None)
 
+    def test_username_is_none(self):
+        """
+        assert get_username method returns a None
+        :return:
+        """
+        self.assertIsNone(self.user.get_username)
+
+    def test_get_email_is_none(self):
+        """
+        assert get_email method returns a None
+        :return:
+        """
+        self.assertIsNone(self.user.get_email)
     #  -->end of User attributes<--
 
 
